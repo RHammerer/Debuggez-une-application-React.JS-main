36b8d668d6fb434f2854a13415ee7c8cf78a134e
rhammerer <hammerer.renaud@gmail.com>
2024-07-22 17:32:25 +0200

    Commit de finalisation

---
+++ src/pages/Home/index.test.js
@@ -0,0 +1,51 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./index";
+
+describe("When Form is created", () => {
+  it("a list of fields card is displayed", async () => {
+    render(<Home />);
+    setTimeout(() => {
+      screen.findByText("Email");
+      screen.findByText("Nom");
+      screen.findByText("Prénom");
+      screen.findByText("Personel / Entreprise");
+
+    }, 101)
+  });
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the success message is displayed", async () => {
+      render(<Home />);
+      fireEvent(
+        await screen.findByText("Envoyer"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+          setTimeout(() => {
+
+             screen.findByText("En cours");
+             screen.findByText("Message envoyé !");
+
+    }, 101)
+    });
+  });
+
+});
+
+
+describe("When a page is created", () => {
+  it("a list of events is displayed", () => {
+    // to implement
+  })
+  it("a list a people is displayed", () => {
+    // to implement
+  })
+  it("a footer is displayed", () => {
+    // to implement
+  })
+  it("an event card, with the last event, is displayed", () => {
+    // to implement
+  })
+});
